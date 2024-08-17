f519e08cb29e13351b1888632b2698db3ac53adc
Petrov Kirill <petrovvkirill@gmail.com>
2024-08-17 14:11:48 +0400

    feat(dev): front init

--- frontend/src/shared/const/routes.ts
+++
@@ -1,8 +0,0 @@
-export enum AppRoutes {
-  HOME = '/',
-  LOGIN = 'login',
-}
-
-export const getHomeRoute = () => AppRoutes.HOME;
-export const getLoginPage = () => `/${AppRoutes.LOGIN}`;
-export const getManagerProductsRoute = () => 'manager/products';
